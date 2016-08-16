096fc0b0b5b451aadfefd7daa2683470fe8be755
NORPG <b10102229@gmail.com>
2016-08-16 17:06:13 +0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Changes to be committed:    (use "git reset HEAD <file>..." to unstage) 	rename stack_manager_pass/active to stack_manager 	modified:   stack_manager/.gitignore 	modified:   stack_manager/automake.mk 	modified:   stack_manager/stack_manager.h

--- stack_manager/automake.mk
+++ stack_manager/automake.mk
@@ -1,17 +1,12 @@
 bin_PROGRAMS += \
-	stack_manager/stack_manager_passive \
-	stack_manager/stack_manager_active
+	stack_manager/stack_manager
 #man_MANS += stack_manager/stack_manager.8
 
-stack_manager_stack_manager_passive_SOURCES = \
+stack_manager_stack_manager_SOURCES = \
 	stack_manager/stack_manager.c \
 	stack_manager/stack_manager.h
-stack_manager_stack_manager_passive_LDADD = lib/libopenflow.a $(FAULT_LIBS) $(SSL_LIBS)
+stack_manager_stack_manager_LDADD = lib/libopenflow.a $(FAULT_LIBS) $(SSL_LIBS)
 
-stack_manager_stack_manager_active_SOURCES = \
-	stack_manager/stack_manager.c \
-	stack_manager/stack_manager.h
-stack_manager_stack_manager_active_LDADD = lib/libopenflow.a $(FAULT_LIBS) $(SSL_LIBS)
 
 #EXTRA_DIST += stack_manager/stack_manager.8.in
 #DISTCLEANFILES += stack_manager/stack_manager.8
